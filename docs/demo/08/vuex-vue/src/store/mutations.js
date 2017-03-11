18515c5214492d92dd42c39ea9e799349e2541a6
quadrant26 <quadrant26@163.com>
2017-03-11 17:02:23 +0800

    vuex store mutations

---
+++ docs/demo/08/vuex-vue/src/store/mutations.js
@@ -0,0 +1,24 @@
+import {
+    INCREMENT,
+    DECREMENT
+} from './types.js'
+import getters from './getters.js'
+
+const state = {
+    count : 20
+};
+
+const mutations = {
+    [INCREMENT](state){
+        state.count++;
+    },
+    [DECREMENT](state){
+        state.count--;
+    }
+}
+
+export default {
+    state,
+    mutations,
+    getters
+}
