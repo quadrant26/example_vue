404ccdba9380abbd0e4d11817f65fd553b0acc3e
quadrant26 <quadrant26@163.com>
2017-03-12 19:22:28 +0800

    router2.x component News

--- docs/example/vue-webpack-2.x/router-vue2.x/src/components/News.vue
+++ docs/example/vue-webpack-2.x/router-vue2.x/src/components/News.vue
@@ -1,5 +1,39 @@
 <template>
     <div id="news">
         我是新闻页
+        <ul>
+            <li>
+                <router-link to="/detail/1" @click="push">新闻1</router-link>
+            </li>
+            <li>
+                <router-link to="/detail/2" @click="replace">新闻2</router-link>
+            </li>
+            <li>
+                <router-link to="/detail/3">新闻3</router-link>
+            </li>
+        </ul>
+
+        <div>
+            <router-view></router-view>
+        </div>
     </div>
 </template>
+
+<script>
+export default {
+    name : 'news',
+    methods : {
+        push(){
+            // router.push({path:'home'});
+            console.log("push");
+        },
+        replace(){
+            console.log("replace");
+        }
+    }
+}
+</script>
+
+<style lang="">
+    ul,li{ list-style:none;}
+</style>
