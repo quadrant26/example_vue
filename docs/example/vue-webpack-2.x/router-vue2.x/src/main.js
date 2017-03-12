6a9e29001438b8db7804197971b55fc461e2e0fc
quadrant26 <quadrant26@163.com>
2017-03-12 19:21:39 +0800

    router2.x entry app

--- docs/example/vue-webpack-2.x/router-vue2.x/src/main.js
+++ docs/example/vue-webpack-2.x/router-vue2.x/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import RouterConfig from './router.config.js'
 
+import './assets/css/animate.css'
+
 Vue.use(VueRouter)
 
 const router = new VueRouter(RouterConfig)
