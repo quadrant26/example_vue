d8ad4456808af5ac56e87daeba3482e01ece2af8
quadrant26 <quadrant26@163.com>
2017-03-12 12:16:22 +0800

    router2.x entry app

---
+++ docs/demo/07/vue-router2.x/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{msg}}</h1>
+    
+    <ul>
+      <li>
+        <router-link to="/home">主页</router-link>
+        <router-link to="/news">新闻</router-link>
+      </li>
+    </ul>
+    <div>
+      <router-view></router-view>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.router-link-active{
+  font-size:20px; color:#f30;
+}
+</style>
