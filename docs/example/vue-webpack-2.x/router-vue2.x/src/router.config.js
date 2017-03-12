a8080bf81fbfefa9c7dcaa29cf671009ad065971
quadrant26 <quadrant26@163.com>
2017-03-12 19:21:29 +0800

    router2.x router-config

--- docs/example/vue-webpack-2.x/router-vue2.x/src/router.config.js
+++ docs/example/vue-webpack-2.x/router-vue2.x/src/router.config.js
@@ -1,10 +1,17 @@
 import Home from './components/Home.vue'
 import News from './components/News.vue'
+import Detail from './components/Detail.vue'
 
 export default {
     routes: [
         { path: '/home', component: Home },
-        { path: '/news', component: News },
+        {
+            path: '/news',
+            component: News,
+            children: [
+                { path: '/detail/:id', component: Detail }
+            ]
+        },
         { path: '*', redirect: '/home' }
     ]
 }
