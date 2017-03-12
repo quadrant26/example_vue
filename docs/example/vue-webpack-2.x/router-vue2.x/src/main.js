24bfa723416ea05da54f6a748325b686c35b6118
quadrant26 <quadrant26@163.com>
2017-03-12 12:14:05 +0800

    router2.x entry

---
+++ docs/example/vue-webpack-2.x/router-vue2.x/src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueRouter from 'vue-router'
+import RouterConfig from './router.config.js'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter(RouterConfig)
+
+new Vue({
+    router,
+    el: '#app',
+    render: h => h(App)
+})
