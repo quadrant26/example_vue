63d5bdb7d9630e7f919813f84ed65aae0cfe94c8
quadrant26 <quadrant26@163.com>
2017-03-15 23:35:08 +0800

    add Loading and axios methods

--- news/vue-news-project/src/main.js
+++ news/vue-news-project/src/main.js
@@ -5,18 +5,40 @@ import VueRouter from 'vue-router'
 import Store from './store/index.js'
 import RouterConfig from './router.config.js'
 
+import axios from 'axios'
+import Loading from './components/loading/index.js'
+
 Vue.use(VueRouter);
+Vue.use(Loading);
+
+// axios 配置
+axios.interceptors.request.use(function(config) {
+    // 配置 请求信息
+    Store.dispatch('showLoading')
+    return config;
+}, function(error) {
+    return Promise.reject(error)
+})
+
+axios.interceptors.response.use(function(response) {
+    Store.dispatch('hideLoading')
+    return response;
+}, function(error) {
+    return Promise.reject(error);
+})
+
 
 const router = new VueRouter({
-  mode:"history",
-  routes:RouterConfig
+    mode: "history",
+    scrollBehavior: () => ({ y: 0 }),
+    routes: RouterConfig
 })
 
 require("./assets/css/base.css")
 
 new Vue({
-  router : router,
-  el: '#app',
-  store: Store,
-  render: h => h(App)
+    router: router,
+    el: '#app',
+    store: Store,
+    render: h => h(App)
 })
