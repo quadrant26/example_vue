3c7cbd67dbe8ab9b148a604c2233402237e51cad
quadrant26 <quadrant26@163.com>
2017-03-15 17:43:39 +0800

    change err config

--- news/vue-news-project/src/router.config.js
+++ news/vue-news-project/src/router.config.js
@@ -3,9 +3,10 @@ import Follow from './components/Follow.vue'
 import Column from './components/Column.vue'
 import UserInfo from './components/UserInfo.vue'
 
-export default[{
-        path : '/home', component: Home,
-        path : '/follow', component: Follow,
-        path : '/column', component: Column,
-        path : '/user-info', component : UserInfo
-}]
+export default[
+    { path : '/home', component: Home },
+    { path : '/follow', component: Follow },
+    { path : '/column', component: Column },
+    { path : '/user-info', component : UserInfo },
+    { path : '*', redirect: '/home'}
+]
