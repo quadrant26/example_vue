8f0fc1f841df72a80ad3b0629ab349e3d9e6495b
quadrant26 <quadrant26@163.com>
2017-03-15 18:36:40 +0800

    add Vuex store

--- news/vue-news-project/src/main.js
+++ news/vue-news-project/src/main.js
@@ -2,11 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 
-//import Store from './store/store'
+import Store from './store/index.js'
 import RouterConfig from './router.config.js'
 
 Vue.use(VueRouter);
-
 
 const router = new VueRouter({
   mode:"history",
