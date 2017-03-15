92cf22e127c9e83c40affc961c9240d6b84642b2
quadrant26 <quadrant26@163.com>
2017-03-15 17:44:37 +0800

    entry main

--- news/vue-news-project/src/main.js
+++ news/vue-news-project/src/main.js
@@ -3,23 +3,14 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 
 //import Store from './store/store'
-//import RouterConfig from './router.config.js'
-import Home from './components/Home.vue'
-import Follow from './components/Follow.vue'
-import Column from './components/Column.vue'
-import UserInfo from './components/UserInfo.vue'
+import RouterConfig from './router.config.js'
 
 Vue.use(VueRouter);
 
-const routes = [{
-    path : '/home', component: Home,
-    path : '/follow', component: Follow,
-    path : '/column', component: Column,
-    path : '/user-info', component : UserInfo
-}]
 
 const router = new VueRouter({
-  routes:routes
+  mode:"history",
+  routes:RouterConfig
 })
 
 require("./assets/css/base.css")
