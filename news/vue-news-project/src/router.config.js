05218b27d9f9d31435898541fa47a736b6f32d9c
quadrant26 <quadrant26@163.com>
2017-03-15 17:15:30 +0800

    router.config.js

---
+++ news/vue-news-project/src/router.config.js
@@ -0,0 +1,11 @@
+import Home from './components/Home.vue'
+import Follow from './components/Follow.vue'
+import Column from './components/Column.vue'
+import UserInfo from './components/UserInfo.vue'
+
+export default[{
+        path : '/home', component: Home,
+        path : '/follow', component: Follow,
+        path : '/column', component: Column,
+        path : '/user-info', component : UserInfo
+}]
