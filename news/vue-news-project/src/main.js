485baa27fa0aa69ed4dd1623b3ac39f15ded32d1
quadrant26 <quadrant26@163.com>
2017-03-15 17:15:17 +0800

    entry main.js

--- news/vue-news-project/src/main.js
+++ news/vue-news-project/src/main.js
@@ -1,7 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+//import Store from './store/store'
+//import RouterConfig from './router.config.js'
+import Home from './components/Home.vue'
+import Follow from './components/Follow.vue'
+import Column from './components/Column.vue'
+import UserInfo from './components/UserInfo.vue'
+
+Vue.use(VueRouter);
+
+const routes = [{
+    path : '/home', component: Home,
+    path : '/follow', component: Follow,
+    path : '/column', component: Column,
+    path : '/user-info', component : UserInfo
+}]
+
+const router = new VueRouter({
+  routes:routes
+})
+
+require("./assets/css/base.css")
 
 new Vue({
+  router : router,
   el: '#app',
   render: h => h(App)
 })
