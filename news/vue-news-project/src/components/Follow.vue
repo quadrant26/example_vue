2842791ed235594f0c7fe3a13b7c2ad4c9a9e53e
quadrant26 <quadrant26@163.com>
2017-03-16 17:25:51 +0800

    update Follow.vue

--- news/vue-news-project/src/components/Follow.vue
+++ news/vue-news-project/src/components/Follow.vue
@@ -1,15 +1,41 @@
 <template>
     <div class="content mt40">
-        123131231231231
+        <ul>
+            <li v-for="(item, index) in arrlist" class="followli">
+                <router-link to="/article">
+                    <h2>{{item.title}}</h2>
+                    <p>{{item.detail}}</p>
+                </router-link>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
     export default{
-        
+        data(){
+            return {
+                arrlist : []
+            }
+        },
+        mounted(){
+            this.getFollow();
+        },
+        methods : {
+            getFollow(){
+                var _this = this;
+                this.$http.get("src/data/follow.data").then(function(res){
+                    //console.log(res.data);
+                    _this.arrlist = res.data;
+                }).catch(function(err){
+                    console.log(err);
+                })
+            }
+        }
     }
 </script>
 
 <style scoped>
     .mt40{ margin-top:40px;}
+    .followli{ padding:5px 10px}
 </style>
