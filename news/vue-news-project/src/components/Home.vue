e20134d4d9df44b1cdaaa6b7592f2aa0778e8ffe
quadrant26 <quadrant26@163.com>
2017-03-16 17:26:29 +0800

    update component for Home

--- news/vue-news-project/src/components/Home.vue
+++ news/vue-news-project/src/components/Home.vue
@@ -3,7 +3,12 @@
         <BannerView></BannerView>
         <div class="newsList">
 	    	<ul>
-                <li v-for="(item, index) in arrList">{{item}}</li>
+                <li v-for="(item, index) in arrList">
+                    <router-link :to="'/article/'+item.id">
+                        <h2>{{index+1}} {{item.title}}</h2>
+                        <p>{{item.detail}}</p>
+                    </router-link>
+                </li>
             </ul>
         </div>
     </div>
@@ -18,7 +23,22 @@
         },
         data(){
             return {
-                arrList : ["apple", "orange", "banana", "juice"]
+                arrList : []
+            }
+        },
+        mounted(){
+            // 获取数据
+            this.getIndex();                 
+        },
+        methods : {
+            getIndex(){
+                var _This = this;
+                this.$http.get("src/data/index.data").then(function (res){
+                    //console.log(res.data);      
+                    _This.arrList = res.data
+                }).catch(function (err){
+                    console.log(err);
+                })
             }
         }
     }
